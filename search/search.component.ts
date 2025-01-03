72ae59d03e984de04306d2e86b06d854211d2272
James <james.distef@gmail.com>
2025-01-03 15:02:36 -0500

    add form to input

--- search/search.component.ts
+++ search/search.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { SelectedAnimalComponent } from '../selected-animal/selected-animal.component';
+import { Component } from "@angular/core";
+import { SelectedAnimalComponent } from "../selected-animal/selected-animal.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-search',
+  selector: "app-search",
   standalone: true,
-  imports: [SelectedAnimalComponent],
-  templateUrl: './search.component.html',
-  styleUrl: './search.component.css',
+  imports: [SelectedAnimalComponent, ReactiveFormsModule],
+  templateUrl: "./search.component.html",
+  styleUrl: "./search.component.css",
 })
 export class SearchComponent {
   isSelected = false;
+  applyForm = new FormGroup({
+    species: new FormControl(""),
+  });
+
+  go() {
+    console.log(this.applyForm.get("species")?.value);
+    this.isSelected = true;
+  }
 }

2c35f984bbdd1577c5661749f4b80242710a9577
James <james.distef@gmail.com>
2025-01-05 16:37:11 -0500

    show real data on search

--- search/search.component.ts
+++ search/search.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { SelectedAnimalComponent } from "../selected-animal/selected-animal.component";
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Animal, SearchService } from "./search.service";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: "app-search",
@@ -8,6 +10,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
   imports: [SelectedAnimalComponent, ReactiveFormsModule],
   templateUrl: "./search.component.html",
   styleUrl: "./search.component.css",
+  providers: [HttpClient],
 })
 export class SearchComponent {
   isSelected = false;
@@ -15,8 +18,20 @@ export class SearchComponent {
     species: new FormControl(""),
   });
 
-  go() {
-    console.log(this.applyForm.get("species")?.value);
+  query: string = "abb";
+  results: any[] = [];
+
+  url = "http://localhost:3000/animals";
+
+  searchService: SearchService = inject(SearchService);
+
+  constructor() {}
+
+  async go() {
+    this.searchService.getAllAnimals().then((animals: Animal[]) => {
+      this.results = animals;
+    });
+    console.log(this.results);
     this.isSelected = true;
   }
 }
--- selected-animal/selected-animal.component.ts
+++ selected-animal/selected-animal.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Animal } from "../search/search.service";
 
 @Component({
-  selector: 'selected-animal',
+  selector: "selected-animal",
   standalone: true,
   imports: [],
-  templateUrl: './selected-animal.component.html',
-  styleUrl: './selected-animal.component.css',
+  templateUrl: "./selected-animal.component.html",
+  styleUrl: "./selected-animal.component.css",
 })
-export class SelectedAnimalComponent {}
+export class SelectedAnimalComponent {
+  @Input() animal!: Animal;
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log(this.animal);
+  }
+}
